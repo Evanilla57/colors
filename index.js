56a5ab77346bb64f05ad221d0c1df1b8e3a5b323
Evanilla57 <grigg005@cougars.csusm.edu>
2023-09-13 13:19:01 -0700

    renamed logo file to shapes

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const inquirer = require('inquirer');
-const fetch = require('node-fetch');
 
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
 // WHEN I have entered input for all the prompts
 // THEN an SVG file is created named `logo.svg`
 
@@ -40,10 +31,4 @@ inquirer
             type: "input",
             name: "shapeColor",
             message: "Enter a color name or hexidecimal number for your logo background shape:",
-        },)
-    // // promises are chained to directly pass inquirer data into fetch request
-    // .then((res) => fetch(`https://api.github.com/users/${res.username}`))
-    // // promises are chained to parse the request for the json data
-    // .then((res) => res.json())
-    // // json data is accepted as user and logged to the console
-    // .then((user) => console.log(user));
+        },);
