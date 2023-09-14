0a5fa8ca28ce8903d55a45fbf35a64f3a4e30d96
Evanilla57 <grigg005@cougars.csusm.edu>
2023-09-13 18:42:46 -0700

    added then and writeFile

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ const shapeMold = require('./shapes');
 
 const fs = require('fs');
 
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-
-
 inquirer
-    .prompt({
+    .prompt([{
         type: "input",
         name: "textInput",
         message: "Enter up to 3 characters for your logo:",
     },
-        {
-            type: "input",
-            name: "textColor",
-            message: "Enter a color name or hexadecimal number for your logo's text:",
-        },
-        {
-            type: "list",
-            name: "shapeInput",
-            message: "Choose a shape for your logo background:",
-            choices: ["Circle", "Square", "Triangle"],
-        },
-        {
-            type: "input",
-            name: "shapeColor",
-            message: "Enter a color name or hexidecimal number for your logo background shape:",
-        },);
+    {
+        type: "input",
+        name: "textColor",
+        message: "Enter a color name or hexadecimal number for your logo's text:",
+    },
+    {
+        type: "list",
+        name: "shapeInput",
+        message: "Choose a shape for your logo background:",
+        choices: ["Circle", "Square", "Triangle"],
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter a color name or hexidecimal number for your logo background shape:",
+    },]
+    ).then((data) => {
+        fs.writeFile("logo.svg", shapeMold(data), (err) =>
+            err ? console.log(err) : console.log("Success!"))
+    });
